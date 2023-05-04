42ec8fee6b5ba7f79214a05c7860d5f45d8f3b15
Rajnish Dargan <rajnish.code30@gmail.com>
2023-05-04 18:13:51 +0530

    Issue #IQ-367 feat: react app for intergration of questionset-editor web componenent

--- web-component-examples/react-app/src/App.js
+++ web-component-examples/react-app/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import { editorConfig } from "./data";
+
+import * as $ from "jquery";
+
+import "@project-sunbird/sunbird-questionset-editor-web-component/sunbird-questionset-editor.js";
+// import "@project-sunbird/sunbird-questionset-editor-web-component/styles.css";
+import "./Sunbird/sunbird-questionset-editor.js"
 
 function App() {
+  const sunbirdQuestionsetEditorRef = useRef(null);
+  window.jQuery = $;
+
+  useEffect(() => {
+    const editorElement = sunbirdQuestionsetEditorRef.current;
+    const handleEditorEvent = (event) => {
+      console.log("Editor Event", event.detail);
+    };
+
+    editorElement.addEventListener("EditorEvent", handleEditorEvent);
+    return () => {
+      editorElement.removeEventListener("EditorEvent", handleEditorEvent);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <lib-questionset-editor
+        editor-config={JSON.stringify(editorConfig)}
+        ref={sunbirdQuestionsetEditorRef}
+      ></lib-questionset-editor>
     </div>
   );
 }
